d2b74c1918fa4c0a15522504b079558290c04504
Daniel Wavamunno <danl.wavah@gmail.com>
2025-04-14 16:52:54 +0300

    initializing the traffic lights pins

---
+++ Trffic Lights MS.cpp
@@ -0,0 +1,7 @@
+// Traffic Light Pins Definitions
+const int trafficLights[4][3] = {
+    {13, 12, 11}, // Red, Yellow, Green for Traffic Light 1
+    {10, 9, 8},   // Red, Yellow, Green for Traffic Light 2
+    {7, 6, 5},    // Red, Yellow, Green for Traffic Light 3
+    {4, 3, 2}     // Red, Yellow, Green for Traffic Light 4
+  };
