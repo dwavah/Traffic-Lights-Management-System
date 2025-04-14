bd8ccd05967784b1944a939a74669ad253fbfe7e
Daniel Wavamunno <danl.wavah@gmail.com>
2025-04-14 16:53:52 +0300

    configuring the ultrasonic trig and echo pins

--- Trffic Lights MS.cpp
+++ Trffic Lights MS.cpp
@@ -5,3 +5,8 @@ const int trafficLights[4][3] = {
     {7, 6, 5},    // Red, Yellow, Green for Traffic Light 3
     {4, 3, 2}     // Red, Yellow, Green for Traffic Light 4
   };
+
+
+    // Ultrasonic sensor Trig and Echo pins (2 per section)
+    const int trigPins[8] = {23, 27, 31, 35, 39, 43, 47, 51}; // Trigger
+    const int echoPins[8] = {22, 26, 30, 34, 38, 42, 46, 50}; // Echo
