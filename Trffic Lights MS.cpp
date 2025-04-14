d990ef6194d74a5666c619fd800f1c99e0f9a613
Daniel Wavamunno <danl.wavah@gmail.com>
2025-04-14 16:58:15 +0300

    initializing the green, yellow and red light timing

--- Trffic Lights MS.cpp
+++ Trffic Lights MS.cpp
@@ -6,13 +6,18 @@ const int trafficLights[4][3] = {
     {4, 3, 2}     // Red, Yellow, Green for Traffic Light 4
   };
 
+  // Ultrasonic sensor Trig and Echo pins (2 per section)
+  const int trigPins[8] = {23, 27, 31, 35, 39, 43, 47, 51}; // Trigger
+  const int echoPins[8] = {22, 26, 30, 34, 38, 42, 46, 50}; // Echo
 
-    // Ultrasonic sensor Trig and Echo pins (2 per section)
-    const int trigPins[8] = {23, 27, 31, 35, 39, 43, 47, 51}; // Trigger
-    const int echoPins[8] = {22, 26, 30, 34, 38, 42, 46, 50}; // Echo
+  // Emergency buzzer pin (passive buzzer)
+  const int buzzerPin = 52;
 
-      // Emergency buzzer pin (passive buzzer)
-    const int buzzerPin = 52;
+  // Distance threshold to detect a car
+  const int vehicleThreshold = 6; // cm (adjusted for small demo lanes)
 
-    // Distance threshold to detect a car
-    const int vehicleThreshold = 6; // cm (adjusted for small demo lanes)
+  // Green light timing
+  const int maxGreenTime = 10000;
+  const int minGreenTime = 5000;
+  const int yellowTime = 2000;
+  const int redBufferTime = 1000;
